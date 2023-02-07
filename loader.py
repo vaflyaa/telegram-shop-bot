c02422e8eb1c4683d9b756c096b1d978b14cfedc
vaflyaa <nikinikolaevich97@gmail.com>
2023-02-07 17:33:24 +0400

    Add Memory storage and path for database

--- loader.py
+++ loader.py
@@ -1,5 +1,9 @@
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from utils.db import DatabaseManager
 from settings import settings
 
-bot = Bot(token=settings.bots.bot_token)
-dp = Dispatcher(bot)
+bot = Bot(token=settings.bots.bot_token, parse_mode=types.ParseMode.HTML)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
+db = DatabaseManager('tg_shop_database.db')
