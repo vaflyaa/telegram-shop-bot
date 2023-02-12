03d20be6642572a888aa486058061477edc2ae9d
vaflyaa <nikinikolaevich97@gmail.com>
2023-02-12 14:04:21 +0400

    Add init

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -3,4 +3,4 @@ from .catalog import dp
 from .admin_settings import dp
 from .admin_questions import dp
 from .help import dp
-
+from .cart import dp
--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,2 +1,3 @@
 from . import categories
 from . import products_from_catalog
+from . import products_from_cart
