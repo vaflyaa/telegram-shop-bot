8f56421f137a24f9edbc829aa838180dd6aa556c
vaflyaa <nikinikolaevich97@gmail.com>
2023-02-15 15:25:30 +0400

    Add init

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -2,5 +2,7 @@ from .menu import dp
 from .catalog import dp
 from .admin_settings import dp
 from .admin_questions import dp
+from .admin_orders import dp
 from .help import dp
 from .cart import dp
+from .order_status import dp
